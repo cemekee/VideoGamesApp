8c56f81b0c672c6c36bfd0198ead217a27e5f796
Cem Eke <cem.eke14@gmail.com>
2021-02-23 13:34:00 +0300

    Initial Commit

---
+++ VideoGamesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoGamesApp
+//
+//  Created by Cem Eke on 23.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
